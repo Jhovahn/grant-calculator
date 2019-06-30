a15b10aaeeafe19d1dbf294a2fa43ab9c569d4ef
Jhovahn Gibbs <nhavohj@gmail.com>
2019-06-30 16:40:53 -0700

    add user input fields

--- src/App.js
+++ src/App.js
@@ -1,26 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      shares: '',
+      outstanding: '',
+      exercisePrice: '',
+      exitValuation: '',
+      years: '',
+      name: ''
+    };
+  }
+
+  handleInput = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  render() {
+    console.log(this.state);
+    return (
+      <div className="App">
+        <h1>Grant Calculator</h1>
+        <ul style={{ listStyleType: 'none' }}>
+          <li>
+            <label>
+              Shares: <span />
+              <input
+                type="text"
+                name="shares"
+                onChange={e => this.handleInput(e)}
+                value={this.state.shares}
+              />
+            </label>
+          </li>
+          <li>
+            <label>
+              Outstanding: <span />
+              <input
+                type="text"
+                name="outstanding"
+                onChange={e => this.handleInput(e)}
+                value={this.state.outstanding}
+              />
+            </label>
+          </li>
+          <li>
+            <label>
+              Exercise Price: <span />
+              <input
+                type="text"
+                name="exercisePrice"
+                onChange={e => this.handleInput(e)}
+                value={this.state.exercisePrice}
+              />
+            </label>
+          </li>
+          <li>
+            <label>
+              Exit Valuation: <span />
+              <input
+                type="text"
+                name="exitValuation"
+                onChange={e => this.handleInput(e)}
+                value={this.state.exitValuation}
+              />
+            </label>
+          </li>
+          <li>
+            <label>
+              Years: <span />
+              <input
+                type="text"
+                name="years"
+                onChange={e => this.handleInput(e)}
+                value={this.state.years}
+              />
+            </label>
+          </li>
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
